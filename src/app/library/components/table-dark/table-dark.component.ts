685486d25baea8ddc75d83e5b42c097fd9dbb111
waberto <waberi.houssein@gmail.com>
2020-07-23 14:46:24 +0200

    :host context with encapsulation

--- src/app/library/components/table-dark/table-dark.component.ts
+++ src/app/library/components/table-dark/table-dark.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-table-dark',
   templateUrl: './table-dark.component.html',
-  styleUrls: ['./table-dark.component.scss']
+  styleUrls: ['./table-dark.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TableDarkComponent implements OnInit {
-
+  @Input() headers: string[];
   constructor() { }
 
   ngOnInit(): void {
